5c5cfb446364a2c207ae098d012ed1f74c2fc45d
BuildTools <unconfigured@null.spigotmc.org>
2021-10-17 16:22:24 +0100

    hopefully this works properly...

--- src/main/java/me/trespo/shutup/Main.java
+++ src/main/java/me/trespo/shutup/Main.java
@@ -1,6 +1,7 @@
 package me.trespo.shutup;
 
 import me.trespo.shutup.response.ShutUp;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -8,9 +9,11 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getServer().getPluginManager().registerEvents(new ShutUp(), this);
+        getServer().getPluginManager().registerEvents(new ShutUp(this), this);
 
     }
+
+
 
     @Override
     public void onDisable() {
--- src/main/java/me/trespo/shutup/response/ShutUp.java
+++ src/main/java/me/trespo/shutup/response/ShutUp.java
@@ -1,24 +1,38 @@
 package me.trespo.shutup.response;
 
+import me.trespo.shutup.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class ShutUp implements Listener {
+
+    Main plugin;
+
+    public ShutUp(Main plugin) {
+        this.plugin = plugin;
+    }
 
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
 
-        if (!(event.getPlayer() instanceof Player)) {
-            event.getPlayer().sendMessage("I only tell players to shut up.");
-            return;
-        }
+        new BukkitRunnable(){
 
-        Player player = event.getPlayer();
+            @Override
+            public void run() {
 
-        player.sendMessage("SHUT UP!");
+                if (!(event.getPlayer() instanceof Player)) {
+                    event.getPlayer().sendMessage("I only tell players to shut up.");
+                    return;
+                }
 
+                Bukkit.broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + "SHUT UP!");
+
+            }
+        }.runTaskLater(plugin, 3);
     }
-
 }
